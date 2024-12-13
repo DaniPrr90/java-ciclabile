b7523662cb5dbf2adc527e79ab2a719a171393c3
DaniPrr90 <danilo.pirro@libero.it>
2024-12-13 12:42:18 +0100

    add:interazione ciclo

--- src/org/lessons/java/ciclabile/Ciclabile.java
+++ src/org/lessons/java/ciclabile/Ciclabile.java
@@ -1,7 +1,7 @@
 package org.lessons.java.ciclabile;
 
 public class Ciclabile {
-    private int[] elements; // Definisco un Array  che contenga gli elementi
+    private int[] elements; // Definisco un Array che contenga gli elementi
     private int currentIndex; // Indice per tracciare la posizione corrente
 
     // Definisco un costruttore che accetta un array di interi
@@ -13,32 +13,39 @@ public class Ciclabile {
         this.currentIndex = 0;
     }
 
-
-//  Definisco un metodo per ottenere l'elemento successivo
-public int getElementoSuccessivo() {
-    if (!hasAncoraElementi()) {
-        throw new IllegalStateException("Non ci sono più elementi da restituire.");
+    // Definisco un metodo per ottenere l'elemento successivo
+    public int getElementoSuccessivo() {
+        if (!hasAncoraElementi()) {
+            throw new IllegalStateException("Non ci sono più elementi da restituire.");
+        }
+        return elements[currentIndex++];
     }
-    return elements[currentIndex++];
+
+    // Metodo per verificare se ci sono ancora elementi
+    public boolean hasAncoraElementi() {
+        return currentIndex < elements.length;
+    }
+
+    // ! Metodo per resettare l'indice
+    public void reset() {
+        currentIndex = 0;
+    }
+
+    // Metodo main
+    public static void main(String[] args) {
+        int[] numeri = { 1, 2, 3, 4, 5 };
+        Ciclabile iteratore = new Ciclabile(numeri);
+
+        while (iteratore.hasAncoraElementi()) {
+            System.out.println(iteratore.getElementoSuccessivo());
+        }
+
+        // ! inserisco il reset dopo l'iteratore
+        iteratore.reset();
+        System.out.println("Iterazione dopo il reset:");
+        while (iteratore.hasAncoraElementi()) {
+            System.out.println(iteratore.getElementoSuccessivo());
+        }
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
- 
-
