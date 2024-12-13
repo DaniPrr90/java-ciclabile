ad851e90c174f74cc49d51d0427c8cfdf598b058
DaniPrr90 <danilo.pirro@libero.it>
2024-12-13 11:19:04 +0100

    add:esercizio ciclabile

--- src/org/lessons/java/ciclabile/Ciclabile.java
+++ src/org/lessons/java/ciclabile/Ciclabile.java
@@ -13,40 +13,6 @@ public class Ciclabile {
         this.currentIndex = 0;
     }
 
-    // Metodo per ottenere l'elemento successivo
-    public int getElementoSuccessivo() {
-        if (!hasAncoraElementi()) {
-            throw new IllegalStateException("Non ci sono più elementi da restituire.");
-        }
-        return elements[currentIndex++];
-    }
-
-    // Metodo per verificare se ci sono ancora elementi
-    public boolean hasAncoraElementi() {
-        return currentIndex < elements.length;
-    }
-
-    // Metodo per resettare l'indice (opzionale, se utile per riavviare l'iterazione)
-    public void reset() {
-        currentIndex = 0;
-    }
-
-    // Metodo main per testare la classe
-    public static void main(String[] args) {
-        int[] numeri = {1, 2, 3, 4, 5};
-        Ciclabile iteratore = new Ciclabile(numeri);
-
-        while (iteratore.hasAncoraElementi()) {
-            System.out.println(iteratore.getElementoSuccessivo());
-        }
-
-        // Esempio di reset dell'iteratore
-        iteratore.reset();
-        System.out.println("Iterazione dopo il reset:");
-        while (iteratore.hasAncoraElementi()) {
-            System.out.println(iteratore.getElementoSuccessivo());
-        }
-    }
 }
  
 
